036950760fe1af2a67def8f676450c2d79020baa
mstailor <tailorms@rknec.edu>
2025-02-22 01:04:16 +0530

    edit [id] page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,7 +2,7 @@ interface UserProfileProps {
     params: { id: string };
 }
 
-export default function UserProfile({ params }: UserProfileProps) {
+export default async function UserProfile({ params }: UserProfileProps) {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
             <div className="w-full max-w-lg p-6 bg-white rounded-2xl shadow-lg text-center">
